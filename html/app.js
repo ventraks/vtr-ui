7fff4c38c73a9331132b6ecf0296a04e06393d21
ventraks <83806248+ventraks@users.noreply.github.com>
2022-03-26 23:01:04 +0100

    Update app.js

--- html/app.js
+++ html/app.js
@@ -1,10 +1,10 @@
 window.addEventListener('message', function(event) {
-    if (event.data.type == "open") {      
+    if (event.data.type == "show") {      
       $("#text").css('background-color', event.data.color)
       $("#text").html(event.data.text)
       $("#container").show().removeClass("slide-out").addClass("container")
 
-    } else if (event.data.type == "close") {
+    } else if (event.data.type == "hide") {
       $("#container").removeClass("container").addClass("slide-out")
     }
 })
